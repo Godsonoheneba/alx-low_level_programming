a44b5eeef6d0484bde063bd09bc48ecb77847434
Godson Oheneba Agyei Dacosta <ohenebadac@gmail.com>
2022-12-08 21:40:57 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                     
- */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-puts("\"Programming is like building a multi lingual puzzle);                                                            
+#include <stdio.h> 
+/**
+ * main - Entry
+ * Return: Always 0
+*/ 
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
