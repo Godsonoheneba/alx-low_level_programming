8cbcf805c9b94490d4b9338e52ddda5910cc8ba7
Godson Oheneba Agyei Dacosta <ohenebadac@gmail.com>
2022-12-15 18:52:47 -0800

    Commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
+ * main - tests function that prints if integer is positive or negative
+ *Return: 0
  */
-int _isupper(int c)
+
+int main(void)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
