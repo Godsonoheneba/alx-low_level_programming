48cc7cebb370b08d6c52c8911efd733217a13d2a
Godson Oheneba Agyei Dacosta <ohenebadac@gmail.com>
2023-02-01 20:30:19 -0800

    initial

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_listint - prints all the elements of a linked list
+* print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
@@ -16,23 +16,3 @@ size_t print_listint(const listint_t *h)
  }
  return (num);
 }
-==========================
-1-listint_len.c
-#include "lists.h"
-/**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
- */
-size_t listint_len(const listint_t *h)
-{
- size_t num = 0;
- while (h)
- {
- num++;
- h = h->next;
- }
- return (num);
-}
-
